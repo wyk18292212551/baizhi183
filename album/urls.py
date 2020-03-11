23e16c55c0038d6d28b30c9aa70e0aadf1e09d7a
wyk <2413315439@qq.com>
2020-03-11 09:58:11 +0800

    添加修改专辑

--- album/urls.py
+++ album/urls.py
@@ -5,4 +5,10 @@ app_name="album"
 
 urlpatterns = [
     path('get_album_data/',views.get_album_data,name='get_album_data'),
+    path('get_chapter_data/',views.get_chapter_data,name='get_chapter_data'),
+    path('edit_album/',views.edit_album,name='edit_album'),
+    path('edit_chapter/',views.edit_chapter,name='edit_chapter'),
+    path('add_album/',views.add_album,name='add_album'),
+    path('add_chapter/',views.add_chapter,name='add_chapter'),
+
 ]
